4293dae6e54c894c37b4fcd2fedb813f5f6ba1cf
0x404f <46415372+Horman69@users.noreply.github.com>
2024-09-15 11:22:33 +0700

    Initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+// About.js
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>Learn more about what we do!</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/Contact.js
@@ -0,0 +1,13 @@
+// Contact.js
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <p>Feel free to reach out to us anytime!</p>
+    </div>
+  );
+}
+
+export default Contact;
